59218e1ed4a4dd0cb7a225a34e36074c14fe9487
patrickgiovanno03 <108338469+patrickgiovanno03@users.noreply.github.com>
2023-12-07 15:01:16 +0700

    V1

---
+++ ProyekiOS/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  ProyekiOS
+//
+//  Created by Patrick Giovanno on 28/11/23.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var judul: UILabel!
+    @IBOutlet weak var tanggal: UILabel!
+    
+}
