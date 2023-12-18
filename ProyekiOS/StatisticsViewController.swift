d42c024c12c1b7916b8c74def04b641839e337ef
Kelvin Sidharta Sie <c14210307@john.petra.ac.id>
2023-12-18 00:00:17 -0800

    feat: add statistics template

---
+++ ProyekiOS/StatisticsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StatisticsViewController.swift
+//  ProyekiOS
+//
+//  Created by Kelvin Sidharta Sie on 17/12/23.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
